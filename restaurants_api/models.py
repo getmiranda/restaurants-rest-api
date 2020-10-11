52d46c81a10d3417d0149b097f6f2fdbb625f187
Jose Manuel Miranda Villagran <androidgalaxys42@gmail.com>
2020-10-10 19:49:52 -0500

    Create Restaurant table

--- restaurants_api/models.py
+++ restaurants_api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    """Database model for restaurants in the system"""
+    id = models.CharField(max_length=36, primary_key=True, null=False, unique=True)
+    rating = models.IntegerField()
+    name = models.CharField(max_length=255)
+    site = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    phone = models.CharField(max_length=10)
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    lat = models.FloatField()
+    lng = models.FloatField()
